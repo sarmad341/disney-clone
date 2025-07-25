3e43f93baed78505108259268488a9d9f90df3dc
Dell <muhammadsarmad2003@gmail.com>
2025-07-25 18:40:05 +0500

    feat: completed full website setup and initial deployment ready

--- app/genre/[id]/page.tsx
+++ app/genre/[id]/page.tsx
@@ -1,3 +1,6 @@
+import MoviesCarousel from "@/components/MoviesCarousel";
+import { getDiscoverMovies } from "@/lib/getMovies";
+
 type Props = {
   params: {
     id: string;
@@ -7,10 +10,16 @@ type Props = {
   };
 };
 
-function GenraPage({ params: { id }, searchParams: { genre } }: Props) {
+async function GenraPage({ params: { id }, searchParams: { genre } }: Props) {
+  const movies = await getDiscoverMovies(id);
+
   return (
-    <div>
-      Welcome to the genre with ID:{id} and name: {genre}
+    <div className="max-w-7xl mx-auto">
+      <div className="flex flex-col space-y-5 mt-32 xl:mt-42">
+        <h1 className="text-6xl font-bold px-10">Results for {genre}</h1>
+
+        <MoviesCarousel title={`Genre`} movies={movies} isVertical />
+      </div>
     </div>
   );
 }
